70b45d81adf3073301207c80878eb53aee1ee6c7
Ualikhan <yalikhan752@gmail.com>
2025-05-26 21:10:36 +0500

    Bugfix

--- server/src/main.ts
+++ server/src/main.ts
@@ -6,6 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     logger: ['error', 'warn', 'log', 'debug'],
   });
+  app.setGlobalPrefix('api');
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors({
     origin: '*',
